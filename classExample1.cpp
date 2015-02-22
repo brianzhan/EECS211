5b01e991bcd3212a8e2f6946658281aea1393fe1
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-02-22 14:27:52 -0600

    Added in human class and fixed weird mistakes from dog class

--- classExample1.cpp
+++ classExample1.cpp
@@ -1,3 +1,7 @@
+/* Class Dog
+  Dog has age, name, weight as variable.
+  There are 2 constructor, one without any parameters, and one with all three parameters
+*/
 class Dog {
   private:
     int * age; 
@@ -7,15 +11,21 @@ class Dog {
   public: 
     // Default constructor
     void Dog() { 
-      this->age = 0;
-      this->name = NULL;
-      this->weight = 0.0;
+      this->age = new int(0);
+      this->name = new String();
+      this->weight = new double(0.0);
     }
     // Constructor with instantiation
     void Dog(int age, String name, double weight) {
-      this->age = age;
-      this->name = name;
-      this->weight = weight;
+      this->age = new int(age);
+      this->name = new String(name);
+      this->weight = new double(weight);
+    }
+    // Destructor
+    void ~Dog() { // Q: Why do you need this? 
+      delete age;
+      delete name;
+      delete weight;
     }
     int getAge() {
       return *(this->age);
@@ -35,4 +45,46 @@ class Dog {
     void setWeight(double weight) {
       *(this->weight) = weight;
     }
+}
+
+/* Class Human */
+// All humans somehow have a pet and all the pets are dogs 
+class human
+{
+  private:
+    int * age;
+    String * name;
+    Dog * pet; // This is the dog class defined above being used.  
+
+  public:
+    // Default constructor
+    void human() {
+      age = new int(0);
+      name = new String();
+      pet = new Dog();
+    }
+    // Constructor with instantiation
+    void human(int age, String name, Dog pet) {
+      age = new int(age);
+      name = new String(name);
+      pet = new Dog(pet.getAge(), pet.getName(), pet.getWeight()); // You need this because there isn't a constructor for Dog that takes in an object Dog.
+    }
+    int getAge() {
+      return *(this->age);
+    }
+    String getName() {
+      return *(this->name);
+    }
+    Dog getPet() {
+      return *(this->pet);
+    }
+    void setAge(int age) {
+      *(this->age) = age;
+    }
+    void setName(String name) {
+      *(this->name) = name;
+    }
+    void setPet(Pet dog) {
+      *(this->dog) = dog;
+    } 
 }
